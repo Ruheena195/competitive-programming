1e0660ad6f1e7daa91d72d2241c5fc3285bd776a
Ruheena195 <ruheen1997@gmail.com>
2023-05-27 22:30:49 +0530

    solved 23rd qn

---
+++ codeforces/problemset_problem_492_A/Vanya_and_Cubes.java
@@ -0,0 +1,19 @@
+//package codeforces.problemset_problem_492_A;
+import java.util.*;
+public class Vanya_and_Cubes {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int sum=1, total=1, height=0;
+
+        for(int i=2;total<=N;i++){
+            height++;
+            sum += i;
+            total += sum;
+        }
+        System.out.println(height);
+
+        //System.out.println(Math.log(N)/Math.log(2));
+    }    
+}
