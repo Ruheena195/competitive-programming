17d223659d044b1ad871393a05168565b032d057
Ruheena195 <ruheen1997@gmail.com>
2023-06-25 19:30:57 +0530

    solved 42nd qn

--- codeforces/problemset_problem_1073_A/Diverse_Substring.java
+++ codeforces/problemset_problem_1073_A/Diverse_Substring.java
@@ -7,27 +7,34 @@ public class Diverse_Substring {
         int n = sc.nextInt();
         String s = sc.next();
 
-        int[] arr = new int[26];
-        StringBuilder sb = new StringBuilder();
+        //int[] arr = new int[26];
+        //StringBuilder sb = new StringBuilder();
 
-        int start=0;
+        //int start=0;
 
-        for(int end=0;end<n;end++){
-            int val = s.charAt(end)-'a';
-            arr[val]++;
-            sb.append(s.charAt(end));
+        for(int i=0;i<n-1;i++){
 
-            while(sb.length()>1 && arr[val]>(sb.length()/2)){
-                arr[s.charAt(start)-'a']--;
-                start++;
-                sb.delete(0,1);
-            }
-
-            if(sb.length()>1){
+            if(s.charAt(i) != s.charAt(i+1)){
                 System.out.println("YES");
-                System.out.println(sb.toString());
+                System.out.println(s.substring(i,i+2));
                 return;
             }
+
+            // int val = s.charAt(end)-'a';
+            // arr[val]++;
+            // sb.append(s.charAt(end));
+
+            // while(sb.length()>1 && arr[val]>(sb.length()/2)){
+            //     arr[s.charAt(start)-'a']--;
+            //     start++;
+            //     sb.delete(0,1);
+            // }
+
+            // if(sb.length()>1){
+            //     System.out.println("YES");
+            //     System.out.println(sb.toString());
+            //     return;
+            // }
         }
         System.out.println("NO");
         
